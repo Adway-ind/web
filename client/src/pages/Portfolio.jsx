32115f2b977f9fdfe74a9b4cc1cc95c2282114d2
Adway Creations <laserktpna@gmail.com>
2026-09-10 14:22:09 +0530

    Update project

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { ArrowUpRight, SlidersHorizontal } from "lucide-react";
 import Video from "../assets/video/portfolio-one.mp4";
 import { API } from "../config/api";
-import SEO from "./components/SEO";
 import { motion } from "framer-motion";
 import Client from "../components/ClientsSection";
 import { Helmet } from "react-helmet-async";
